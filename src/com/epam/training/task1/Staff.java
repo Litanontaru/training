96fbb09a4aff545870ffde41b8d799fa6ad071a5
Poul <you@example.com>
2017-09-29 14:42:49 +0400

    created staff class

---
+++ src/com/epam/training/task1/Staff.java
@@ -0,0 +1,24 @@
+package com.epam.training.task1;
+
+public abstract class Staff {
+
+    private String name;
+    private int age;
+
+    Staff(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
