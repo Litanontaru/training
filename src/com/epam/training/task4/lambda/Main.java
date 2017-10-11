ee524370c30639e54d6c8707a0c862657bb6d904
Poul <you@example.com>
2017-10-11 14:34:13 +0400

    deleted old imports

--- src/com/epam/training/task4/lambda/Main.java
+++ src/com/epam/training/task4/lambda/Main.java
@@ -1,13 +1,5 @@
 package com.epam.training.task4.lambda;
 
-import javafx.util.Pair;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.Period;
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class Main {
 
     public static void main(String[] args) {
